fce8f66718b3f4e5cdd955016a3867e24b2c6c11
Azza <azzagamel11@gmail.com>
2024-04-21 22:25:58 +0200

    almost done from design

--- src/app/Components/Header/header.component.ts
+++ src/app/Components/Header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './Navbar/navbar.component';
+import { ContentComponent } from './headerContent/content.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,ContentComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './Components/Footer/footer.component';
 import { HeaderComponent } from './Components/Header/header.component';
+import { ItemsSectionComponent } from './Components/ContentSection/items-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent,HeaderComponent],
+  imports: [RouterOutlet,FooterComponent,HeaderComponent,ItemsSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
